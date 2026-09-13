116023c0a08ae6e26710f0e0e65d251bb4c53bcd
thyuen <40354641+thyuen@users.noreply.github.com>
2026-09-13 10:54:52 +1000

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,32 @@
 var addressPoints = [
   [
     "Melbourne, Australia",
-    -37.8206284,
-    144.9500129
+    -37.8142454,
+    144.9631732
   ],
   [
-    "Incheon, Korea",
-    37.390658,
-    126.6350104
+    "Incheon, South Korea",
+    37.456,
+    126.7052
+  ],
+  [
+    "Sydney, Australia",
+    -33.8698439,
+    151.2082848
+  ],
+  [
+    "Hong Kong, Hong Kong",
+    22.2818333,
+    114.1582831
+  ],
+  [
+    "Singapore, Singapore",
+    1.2899175,
+    103.8519072
+  ],
+  [
+    "Nadi, Fiji",
+    -17.7992725,
+    177.4178549
   ]
 ];
